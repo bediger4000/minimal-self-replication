1036e43f171b1aa6351145e6209d7f14dc8f00b4
Bruce Ediger <bediger8@gmail.com>
2026-02-21 07:14:35 -0700

    fix a bug. Why did this last so long?

--- interpolate/variables_test.go
+++ interpolate/variables_test.go
@@ -72,7 +72,7 @@ func TestVariables(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Variables(tt.args.dict, tt.args.originalString); got != tt.want {
+			if got := Variables(tt.args.originalString, tt.args.dict); got != tt.want {
 				t.Errorf("Variables() = %v, want %v", got, tt.want)
 			}
 		})
